eead57f925bbedc321d44cc136b6f7956e33e020
chopsuey5000 <chopsuey5000@gmail.com>
2015-04-27 18:18:14 +0200

    firmware added

---
+++ esp8266-to-IoT.ino
@@ -0,0 +1,62 @@
+#define SSID "your_SSID"
+#define PASS "your_pass"
+#define IP "184.106.153.149" // thingspeak.com IP
+String GET = "GET /update?key=L04DRVRSERUC44OR&field1=";
+
+long randNumber;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("AT");
+  delay(5000);
+  if (Serial.find("OK")) {
+    connectWiFi();
+  }
+}
+
+void loop() {
+  char buffer[10];
+  randNumber = random(1,100);
+  String tempF = dtostrf(randNumber, 4, 1, buffer);
+  updateData(tempF);
+  delay(60000);
+}
+
+void updateData(String data) {
+  String cmd = "AT+CIPSTART=\"TCP\",\"";
+  cmd += IP;
+  cmd += "\",80";
+  Serial.println(cmd);
+  delay(2000);
+  if (Serial.find("Error")) {
+    return;
+  }
+  cmd = GET;
+  cmd += data;
+  cmd += "\r\n";
+  Serial.print("AT+CIPSEND=");
+  Serial.println(cmd.length());
+  if (Serial.find(">")) {
+    Serial.print(cmd);
+  } else {
+    Serial.println("AT+CIPCLOSE");
+  }
+}
+
+boolean connectWiFi() {
+  Serial.println("AT+CWMODE=1");
+  delay(2000);
+  String cmd = "AT+CWJAP=\"";
+  cmd += SSID;
+  cmd += "\",\"";
+  cmd += PASS;
+  cmd += "\"";
+  Serial.println(cmd);
+  delay(5000);
+  if (Serial.find("OK")) {
+    return true;
+  } else {
+    return false;
+  }
+}
